e587b369c87ef8ec14ae757b9ccdc3eb50d429bf
Yi Zhang <sheva114@gmail.com>
2020-08-14 23:33:32 -0400

    add more explanation and try super

--- orm.py
+++ orm.py
@@ -20,6 +20,7 @@ class StringField(Field):
 #need type class to be parent class
 class ModelMetaclass(type):
     def __new__(cls, name, bases, attrs):
+        print("it's here")
         if name == 'Model':
             return type.__new__(cls, name, bases, attrs)
         __mappings__ = {}
@@ -63,17 +64,27 @@ class Model(dict, metaclass = ModelMetaclass):
 class User(Model):
     id = IntegerField('id')
     name = StringField('Username')
-    email = StringField('email')
+    Email = StringField('email')
     password = StringField('password')
-
-u = User(id='12345', name='Michael', email='test@orm.org', password='my-pwd')
+print('====================')
+print(User.__dict__)
+u = User(id='12345', name='Michael', Email='test@orm.org', password='my-pwd')
 u.ceo = 'jack'
-print(u.test)
+print(u.ceo)
 u.save()
 
 
 
 '''
 为什么要把attr里面和Field相关的属性删掉呢
+
 首先实例的属性会覆盖类的属性，但是因为我们user的类没有id这样的属性
+class User下面的id的定义只是定义field，然后u = user（。。。）的时候是定义了dictionary，但是initialize object的paramter要和class的variable一样, 因为metaclass在define user的时候就生效了而不是到initilization的时候
+这时候class User下面的variable name就已经插入到mapping里面，如果u = User（。。。）parameter不一样的话就会getattrt到None了
+
+如果不删的话getattr会先去看u的__dict__然后没有，就会去call User的__dict__,然后拿到不对的。因为没有删
+如果删掉的话两个__dict__都拿不到，就会去call default的__getattr__这时候就直接call dict[key]拿到对的value了
+
+User的initialization是和dict一样，有[key]没有u.id
+参考资料：https://www.cnblogs.com/ArsenalfanInECNU/p/9100874.html
 '''
