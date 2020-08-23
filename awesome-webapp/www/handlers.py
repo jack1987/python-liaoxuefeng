020232837bec63f120cef7db2b264549f3d5951a
Yi Zhang <sheva114@gmail.com>
2020-08-22 22:45:08 -0400

    add users api

--- awesome-webapp/www/handlers.py
+++ awesome-webapp/www/handlers.py
@@ -6,6 +6,19 @@ from coroweb import get, post
 
 from models import User, Comment, Blog, next_id
 
+from apis import Page
+
+
+def get_page_index(page_str):
+    p = 1
+    try:
+        p = int(page_str)
+    except ValueError as e:
+        pass
+    if p < 1:
+        p = 1
+    return p
+
 @get('/')
 async def index(request):
     summary = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
@@ -18,3 +31,16 @@ async def index(request):
         '__template__': 'blogs.html',
         'blogs': blogs
     }
+
+## 获取用户信息API
+@get('/api/users')
+async def api_get_users(*, page='1'):
+    page_index = get_page_index(page)
+    num = await User.findNumber('count(id)')
+    p = Page(num, page_index)
+    if num == 0:
+        return dict(page=p, users=())
+    users = await User.findAll(orderBy='created_at desc', limit=(p.offset, p.limit))
+    for u in users:
+        u.passwd = '******'
+    return dict(page=p, users=users)
